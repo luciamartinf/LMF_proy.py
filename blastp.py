dd74a127c9193e0f72ecd64a628eb13303214a29
luciamartinf <56353778+luciamartinf@users.noreply.github.com>
2020-06-21 13:11:34 +0200

    Update blastp.py

--- blastp.py
+++ blastp.py
@@ -11,56 +11,38 @@ multifasta = sys.argv[2] #subject
 
 
 def blast(fasta, multifasta, output_blast, coverage=str(50), identity=str(30)):
+    
+    """
+        Ejecuta blastp con un evalue predeterminado de 0.00001 
+        Después filtra el resultado con la cobertura y la identidad predeterminadas 
+        como 50 y 30 respectivamente aunque pueden ser definidas por el usuario.
+        Devuelve un fichero tipo tsv con el resultado del blast que por cada hit con la query incluye:
+        - identificador secuencia subject
+        - identidad 
+        - cobertura
+        - evalue
+        - secuencia subject
+    """
 
     with open(output_blast, "w") as salida:
 
         command_line = ['blastp', '-query', fasta,
                         '-subject', multifasta,
-                        '-evalue', '0.000001',
+                        '-evalue', '0.00001',
                         '-outfmt', '6 sseqid pident qcovs evalue sseq']
         blastp = subprocess.Popen(command_line,
-                                  stdout=subprocess.PIPE)
-        #"awk -v identity=identity '$2 > identity {print}'"
+                                  stdout = subprocess.PIPE)
 
         awk = subprocess.Popen(['/usr/bin/awk','-v','identity='+identity,
                                 '$2 >= identity {print}'],
-                               stdin=blastp.stdout,
-                               stdout=subprocess.PIPE)
+                               stdin = blastp.stdout,
+                               stdout = subprocess.PIPE)
 
-        awk2=subprocess.Popen(['/usr/bin/awk','-v','coverage='+coverage,
+        awk2=subprocess.Popen(['/usr/bin/awk',
+                               '-v','coverage='+coverage,
                                '$3 >= coverage {print}'],
-                              stdin=awk.stdout,
-                              stdout=salida)
+                              stdin = awk.stdout,
+                              stdout = salida)
         awk2.wait()
 
     return output_blast
-
-
-#try:
-    #identity = sys.argv[4]
-    #coverage = sys.argv[3]
-    #blastp(fasta, multifasta, coverage, identity)
-#except:
-    #try:
-        #identity = sys.argv[4]
-        #blastp(fasta, multifasta, identity, coverage=str(50))
-    #except:
-        #try:
-            #coverage = sys.argv[3]
-            #blastp(fasta, multifasta, coverage, identity=str(30))
-        #except:
-            #blastp(fasta, multifasta, coverage=str(50), identity=str(30))
-
-#probar a poner los try dentro de blastp
-
-
-
-
-
-#seqs = ['P00519', 'P05480', 'P12931']
-
-#for seq in seqs:
-      #command_line = ['blastp','-query',
-                #seq + '.fasta','-out',
-                #seq + '_blout', '-outfmt',
-                #'6','-db','nr.00']
