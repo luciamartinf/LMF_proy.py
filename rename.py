273d52c25d13fd6d00a7544e5c18d875ddc920b1
luciamartinf <56353778+luciamartinf@users.noreply.github.com>
2020-06-21 14:39:18 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -4,13 +4,22 @@
 import re
 
 def renom(dic, filename, rename):
+	
+	"""
+		Cambia el accession por el nombre común del organismo
+		  	Input = diccionario con el accession y el nombre común de los organismos,
+			fichero que queremos renombrar, nombre del fichero renombrado.
+	"""
+
 	file = open(filename, "r")
 	filetext = file.read()
 	file.close()
+	
 	with open(rename, "w") as f:
 		for org in dic:
 			if re.search(org, filetext):
 				filetext = filetext.replace(org, dic[org])
 				filetext = filetext.replace("@"," | ")
 		print(filetext, file = f)
+		
 	return rename
