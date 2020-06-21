8ece1267845de7c84a13e004fd25e095fcc4a8d3
luciamartinf <56353778+luciamartinf@users.noreply.github.com>
2020-06-21 19:05:23 +0200

    Update myprosite.py

--- myprosite.py
+++ myprosite.py
@@ -6,14 +6,31 @@ import re
 from Bio import Seq
 from Bio import SeqIO
 
+
+"""
+    Para la ejecución de este paquete es necesario el fichero "prosite.dat"
+    en la carpeta desde la que se está ejecutando el programa
+"""
+
+
 def create_dic_dominios():
-    dic_dominios={}
+    
+    """
+        Crea un diccionario con los patrones de la base de datos de 
+        prosite transformados para que pueda utilizarlos el módulo re
+        y el accession correspondiente a cada patrón
+        
+    """
 
     with open("prosite.dat","r") as handle:
+        
         records = Prosite.parse(handle)
+        
         for record in records:
+            
             accession = record.accession
             pattern = record.pattern
+            
             if pattern == "":
                 continue
             else:
@@ -25,47 +42,73 @@ def create_dic_dominios():
                 pattern = pattern.replace('x', '.')
                 pattern = pattern.replace('>', '$')
                 pattern = pattern.replace('<', '^')
-                #print(pattern)
-                #print(accession)
 
             dic_dominios[pattern] = accession
 
+            
     return dic_dominios
 
 
-#accesion=dic_dominios[pattern]
-#accesion=dic_dominios.get(accesion,None)
-#patrones=dic_dominios.keys()
 
 def search_pattern(dic, input, output):
-    #file = open(input, "r")
-    #filetext = file.read()
-    #file.close()
+    
+    """
+        Busca cada patrón que encuentra en el diccionario en las
+        secuencias proteicas que encuentra en el fasta que hace de input. 
+        Devuelve un fichero tipo txt con información sobre los dominios 
+        encontrados en cada una de las proteínas. 
+    """
+    
     with open(input, "r") as input_handle:
+        
         with open(output, "w") as output_handle:
+            
             for record in SeqIO.parse(input_handle, "fasta"):
+                
                 prot_id = record.id
                 prot_seq = record.seq
                 prot = str(prot_seq)
 
                 print("\n>"+prot_id+"\n", file = output_handle)
-                #print(prot_seq+"\n", file = output_handle)
-                #no se si imprimir tb la secuencia
+         
                 for pattern in dic:
+                
                     accession = dic[pattern]
+                    
                     if re.search(pattern, prot):
+                        
                         with open("prosite.dat","r") as handle:
+                            
                             records = Prosite.parse(handle)
+                            
                             for record in records:
+                                
                                 accession_2 = record.accession
+                                
                                 if (accession_2 == accession):
+                                    
                                     name = record.name
                                     description = record.description
                                     pattern_2 = record.pattern
-                                    print("\tDominio: "+name+" | "+accession, file = output_handle)
-                                    print("\tDescripción: "+description, file = output_handle)
-                                    print("\tPatrón: "+pattern_2+"\n", file = output_handle)
-
-            #for p in patrones:
-                #if re.match(p, donde lo buscas)
+                                    
+                                    print("\tDominio: "+name+" | "+accession, 
+                                          file = output_handle)
+                                    print("\tDescripción: "+description, 
+                                          file = output_handle)
+                                    print("\tPatrón: "+pattern_2, 
+                                          file = output_handle)
+                                    
+                                    match = re.finditer(pattern, prot)
+                                    
+                                    for m in match:
+                                        start = m.start()
+                                        end = m.end()
+                                        
+                                        print("\t- Posición: "
+                                              +str(start)+" - "+str(end), 
+                                              file = output_handle)
+                                    
+                                    print("", file = output_handle)
+            
+            
     return output
