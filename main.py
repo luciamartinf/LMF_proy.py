c39eabdaf7dc5780b3ec61be23b5072aeee6aa44
luciamartinf <56353778+luciamartinf@users.noreply.github.com>
2020-06-21 19:10:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,23 +9,31 @@ from blastp import blast
 from muscle import muscle, prep_muscle, muscle_tree
 from rename import renom
 from myprosite import create_dic_dominios, search_pattern
+import shutil
 #from tree import tree
 
 def main():
-    carpeta = sys.argv[1] #secuencias_y_...
-    querys = sys.argv[2] #PBPs_query.fa
+    #[1] secuencias_y_...
+    #[2] PBPs_query.fa
+
+    name_proy = input("Nombre del proyecto (sin blancos): ")
+
+    os.mkdir(name_proy)
+    results = "{}/resultados".format(name_proy)
+    os.mkdir(results)
+
+    secuencias_fasta = "{}/secuencias.fasta".format(name_proy)
 
     dic_dominios = create_dic_dominios()
-
-    i=1
-    while True :
-        secuencias_fasta = "secuencias_{}.fasta".format(i)
-
-        if not os.path.isfile(secuencias_fasta):
-            break
-        i+=1
-
     dic_org = {}
+    #i=1
+    #while True :
+        #secuencias_fasta = "secuencias_{}.fasta".format(i)
+
+        #if not os.path.isfile(secuencias_fasta):
+            #break
+        #i+=1
+    carpeta = sys.argv[1]
 
     for gbks in os.listdir(carpeta):
         file = os.path.join(carpeta,gbks)
@@ -38,12 +46,20 @@ def main():
     #return secuencias.fasta = output_gbk
     #output_gbk = "secuencias_{}.fasta".format(i)
 
+    querys = sys.argv[2]
+
     with open(querys, "r") as query_handle:
         for record in SeqIO.parse(query_handle, "fasta"):
             seq = record.seq
             id = record.id
-            query = "{}.fasta".format(id)
-            output_blast = "blast_{}.tsv".format(id)
+
+            folder = "{}/{}".format(results, id)
+            if os.path.isdir(folder) == True:
+                shutil.rmtree(folder, ignore_errors=True)
+            os.mkdir(folder)
+
+            query = "{}/{}.fasta".format(folder, id)
+            output_blast = "{}/blast_{}.tsv".format(folder, id)
             with open(query, "w") as q:
                 print(">{}\n{}\n".format(id,seq), file=q)
             try:
@@ -61,24 +77,24 @@ def main():
                     except:
                         blast(query, secuencias_fasta, output_blast)
 
-            input_muscle = "muscle_{}.fa".format(id)
+            input_muscle = "{}/muscle_{}.fa".format(folder, id)
             prep_muscle(output_blast, query, input_muscle)
-            rename_muscle = "muscle_{}_rn.fa".format(id)
+            rename_muscle = "{}/muscle_{}_rn.fa".format(folder, id)
             renom(dic_org, input_muscle, rename_muscle)
 
-            align = "align_{}.fa".format(id)
+            align = "{}/align_{}.fa".format(folder, id)
             muscle(input_muscle, align)
-            rename_align = "align_{}_rn.fa".format(id)
+            rename_align = "{}/align_{}_rn.fa".format(folder, id)
             renom(dic_org, align, rename_align)
 
-            tree = "tree_{}.nw".format(id)
+            tree = "{}/tree_{}.nw".format(folder, id)
             muscle_tree(align, tree)
-            rename_tree = "tree_{}_rn.nw".format(id)
+            rename_tree = "{}/tree_{}_rn.nw".format(folder, id)
             renom(dic_org, tree, rename_tree)
 
-            dominios = "dominios_{}.txt".format(id)
+            dominios = "{}/dominios_{}.txt".format(folder, id)
             search_pattern(dic_dominios, input_muscle, dominios)
-            rename_dominios = "dominios_{}_rn.txt".format(id)
+            rename_dominios = "{}/dominios_{}_rn.txt".format(folder, id)
             renom(dic_org, dominios, rename_dominios)
 
     return rename_dominios
