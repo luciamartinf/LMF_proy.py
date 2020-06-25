eb4614239a9ef3630f2a0a5150e0c4dd1077365d
luciamartinf <56353778+luciamartinf@users.noreply.github.com>
2020-06-25 17:35:11 +0200

    Update help.py

--- help.py
+++ help.py
@@ -46,3 +46,35 @@ def helpme(parser, name_proy):
     parser.print_help()
     
     sys.exit(1)
+    
+    
+    
+def check_proyecto(name_proy):
+
+    """
+        Es una función recursiva. 
+        Comprueba que no exista una carpeta con el mismo nombre
+        de proyecto introducido. 
+    """
+    
+    if os.path.isdir(name_proy):
+        
+        print("Ya existe una carpeta con el nombre del proyecto \
+        dado anteriormente.")
+        print("Si no desea eliminarla, podrá introducir \
+        un nombre diferente para el proyecto")
+
+        check = input("¿Desea eliminar la carpeta existente (s/n)?" )
+
+        if (check.lower() == "s"):
+            shutil.rmtree(name_proy, ignore_errors=True)
+            
+        elif (check.lower() == "n"):
+            name_proy = input("Introduzca un nombre para el proyecto \
+            diferente (sin blancos): ")
+            check_proyecto(name_proy)
+            
+        else:
+            print("No se ha introducido una opción válida, por favor \
+                  vuelva a ejecutar el programa")
+            sys.exit(1)
